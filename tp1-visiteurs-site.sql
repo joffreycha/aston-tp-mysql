47a91dc20f93ebbed2388c6b29fdee3f73df4687
Joffrey Chassat <joffrey.chassat@hotmail.fr>
2020-03-18 16:40:00 +0100

    Fix nb_visiteur view & auto-increment

--- tp1-visiteurs-site.sql
+++ tp1-visiteurs-site.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE `site` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
 CREATE TABLE `visiteur` (
-  `id_visiteur` int NOT NULL,
+  `id_visiteur` int NOT NULL AUTO_INCREMENT,
   `ip_visiteur` varchar(45) NOT NULL,
   `date_premiere_visite_visiteur` datetime DEFAULT CURRENT_TIMESTAMP,
   `date_derniere_visite_visiteur` datetime DEFAULT NULL,
@@ -10,4 +10,4 @@ CREATE TABLE `visiteur` (
   UNIQUE KEY `id_visiteur_UNIQUE` (`id_visiteur`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `nombre_de_visiteurs` AS select count(`visiteur`.`nombre_visites`) AS `nombre_de_visiteurs` from `visiteur`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `nombre_de_visiteurs` AS select count(`visiteur`.`id_visiteur`) AS `nombre_de_visiteurs` from `visiteur`;
