0d300a364d7a5892cc20ad6b3399688161c707f6
Joffrey Chassat <joffrey.chassat@hotmail.fr>
2020-03-20 14:30:41 +0100

    Add Q29 art.type orderby evolution btw 2015&2016

--- beer/tp_beer.sql
+++ beer/tp_beer.sql
@@ -235,5 +235,30 @@ HAVING ABS(((QUANTITE_VENDUE_2016 - QUANTITE_VENDUE_2015) / QUANTITE_VENDUE_2016
 
 -- 29. Lister les types de bières suivant l’évolution de leurs ventes entre 2015 et 2016.
 -- Classer le résultat par ordre décroissant des performances.
+SELECT NOM_TYPE, 
+(	# ventes 2016 - ventes 2015 = evolution des ventes
+	(
+		SELECT SUM(QUANTITE)
+		FROM ventes
+		INNER JOIN article using(ID_ARTICLE)
+		WHERE article.ID_TYPE = type.ID_TYPE
+		AND ANNEE = 2016
+	) -
+	(
+		SELECT SUM(QUANTITE)
+		FROM ventes
+		INNER JOIN article using(ID_ARTICLE)
+		WHERE article.ID_TYPE = type.ID_TYPE
+		AND ANNEE = 2015
+	) 
+) AS EVOLUTION
+FROM type
+GROUP BY ID_TYPE
+ORDER BY EVOLUTION DESC
+;
+
 -- 30. Existe-t-il des tickets sans vente ?
+
+
 -- 31. Lister les produits vendusen 2016 dans des quantités jusqu’à 15% des quantités de l’article le plus vendu.
+
